169462b5e89b5b71d698c932be754a5796fab44d
Tikaram <tikarammahato377@gmail.com>
2024-09-05 17:25:10 +0530

    second commit

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-const Filter = ({filterData}) => {
+const Filter = ({ filterData }) => {
   return (
-    <div>
+    <div className="w-11/12 flex flex-wrap max-w-max gap-y-4 space-x-4 mx-auto py-4 justify-centre">
       {filterData.map((data) => {
-        return(<button>
+        return (
+        <button 
+          className={`text-lg px-2 py-2 rounded-md font-medium text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300`}
+        >
           {data.title}
         </button>)
-      }) }
+      })}
     </div>
   );
 }

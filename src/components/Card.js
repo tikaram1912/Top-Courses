8a9d4cb8f305f396473a58bd6e8fa316c924d032
Tikaram <tikarammahato377@gmail.com>
2024-09-06 14:25:18 +0530

    updation

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,18 +3,19 @@ import { FcLike } from 'react-icons/fc';
 
 const Card = ({course}) => {
     return (
-        <div>
-            <div>
+        <div className="w-[300px] bg-bgDark bg-opacity-80 rounded-md overflow-hidden">
+            <div className="relative">
                 <img src = { course.image.url}></img>
-                <div>
+                <div className="w-[40px] h-[40px] bg-white rounded-full absolute right-2 bottom-3 grid place-items-center">
                     <button>
                         <FcLike fontSize = "1.75rem"/>
                     </button>
                 </div>
-                <div>
-                    <p>{course.tilte}</p>
-                    <p>{course.description}</p>
-                </div>
+                
+            </div>
+            <div className="p-4">
+                    <p className="text-white font-semibold text-lg leading-6">{course.title}</p>
+                    <p className="mt-2 text-white">{course.description}</p>
             </div>
         </div>
     );

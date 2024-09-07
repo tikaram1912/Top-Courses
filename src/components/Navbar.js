1e37ae7039e886e4182e9b94c1f8f315564c23da
Tikaram <tikarammahato377@gmail.com>
2024-09-07 21:40:50 +0530

    updation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
-  return (
-    <div>
+  return(
+    <div >
       <nav className="bg-bgDark py-4">
-        <h1 className="text-3xl font-bold text-center text-white">Top Courses</h1>
+        <h1 className="text-3xl font-bold text-center text-white">
+          Top Courses
+        </h1>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;

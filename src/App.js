91a589d1d95a189e73a8be6b9a819dbf1f1e5cdb
Tikaram <tikarammahato377@gmail.com>
2024-09-07 21:24:42 +0530

    Updation

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
 
   const [courses, SetCourses] = useState("");
   const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,18 +32,20 @@ const App = () => {
   }, []);
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-bgDark2">
       <div><Navbar /></div>
       <div className="bg-bgDark2">
         <div>
           <Filter
             filterData={filterData}
+            category={category}
+            setCategory={setCategory}
           />
         </div>
         <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
           {
             loading ? <Spinner /> : <Cards
-              courses={courses}
+              courses={courses} category = {category}
             />
           }
         </div>

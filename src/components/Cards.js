8833983b4131ae427a9b643c614e14726748fc7e
Tikaram <tikarammahato377@gmail.com>
2024-09-07 21:39:35 +0530

    updation

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,24 +1,34 @@
 import React from "react";
 import Card from "./Card";
+import { useState } from "react";
 
-const Cards = ({courses}) => {
+const Cards = (props) => {
+  let courses = props.courses;
+  let category = props.category;
+  const [likedCourses, setLikedCourses] = useState([]);
 
   let allCourses = [];
 
   //returns you a list of all courses received from the api response
   const getCourses = () => {
-    Object.values(courses).forEach ((courseCategory) => {
-      courseCategory.forEach((course) => {
-        allCourses.push(course);
+    if(category == "All"){
+      Object.values(courses).forEach ((courseCategory) => {
+        courseCategory.forEach((course) => {
+          allCourses.push(course);
+        })
       })
-    })
-    return allCourses;
+      return allCourses;
+    }
+    else{
+      //category wise data array pass krega
+      return courses[category];
+    }
   }
 
   return(
     <div className="flex flex-wrap justify-center gap-4 mb-4" >
        {getCourses().map( (course) => {
-        return <Card course = { course} />
+        return <Card course = { course} likedCourses = {likedCourses} setLikedCourses = {setLikedCourses}/>
        })}
     </div>
   );
